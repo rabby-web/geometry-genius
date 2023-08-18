27a6470c443c26f0e6b180667bf04d0e9ecf9c9b
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-18 08:21:16 +0600

    rectangle are

--- script/area.js
+++ script/area.js
@@ -1,1 +1,16 @@
-console.log(2);
+function calculateTriangleArea() {
+  const baseFiled = document.getElementById("triangle-b");
+  const baseValueText = baseFiled.value;
+  const base = parseFloat(baseValueText);
+  console.log(base);
+
+  const hightFiled = document.getElementById("triangle-h");
+  const hightValueText = hightFiled.value;
+  const hight = parseFloat(hightValueText);
+  console.log(hight);
+
+  const area = 0.5 * base * hight;
+  console.log(area);
+  const areaSet = document.getElementById("triangle-area");
+  areaSet.innerText = area;
+}
