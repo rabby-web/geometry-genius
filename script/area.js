eb8bdd8afbcb8cc5600278c60a0930f94e626152
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-18 08:50:59 +0600

    rectangle setup

--- script/area.js
+++ script/area.js
@@ -1,3 +1,10 @@
+function getInputValue(fieldId) {
+  const inputField = document.getElementById(fieldId);
+  const inputValueText = inputField.value;
+  const value = parseFloat(inputValueText);
+  return value;
+}
+
 function calculateTriangleArea() {
   const baseFiled = document.getElementById("triangle-b");
   const baseValueText = baseFiled.value;
@@ -13,4 +20,14 @@ function calculateTriangleArea() {
   console.log(area);
   const areaSet = document.getElementById("triangle-area");
   areaSet.innerText = area;
+}
+
+function calculateRectangleArea() {
+  const rectangleWidth = getInputValue("rectangle-w");
+  const rectangleHeight = getInputValue("rectangle-i");
+
+  const area = rectangleHeight * rectangleHeight;
+  const areaSet = document.getElementById("rectangle-area");
+  areaSet.innerText = area;
+  console.log(rectangleWidth, rectangleHeight, area);
 }
